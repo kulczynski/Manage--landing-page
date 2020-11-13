2b0d72e0cf5326cd52746d9c1cb827fe196b808f
tomy <kulczynski.t@gmail.com>
2020-11-14 00:11:10 +0100

    work in progress

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const body = document.querySelector('body');
     const hamburgerBtn = document.querySelector('.hamburger');
     const navMenu = document.querySelector('.navigation--mobile');
     const header = document.querySelector('header');
@@ -8,10 +9,11 @@ document.addEventListener("DOMContentLoaded", () => {
         if (navMenu.classList.contains('roll')) {
             navMenu.classList.remove('roll');
             header.classList.add('bgd');
+            body.classList.add('blocked-y');
         } else {
-
             navMenu.classList.add('roll');
             header.classList.remove('bgd');
+            body.classList.remove('blocked-y');
         }
     })
 
